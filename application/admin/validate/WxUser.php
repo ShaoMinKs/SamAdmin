415bd18a900bced1a1ad20e1464c8cf25ce44c1a
sam <sam_zz@126.com>
2019-12-07 00:44:30 +0800

    update application/admin/validate/WxUser.php.

--- application/admin/validate/WxUser.php
+++ application/admin/validate/WxUser.php
@@ -5,18 +5,19 @@ use think\Validate;
 class WxUser extends Validate{
 
     protected $rule = [
-        'wxname'     => 'require',
+        'wxname'    => 'require|token',
         'appid'     => 'require',
-        'appsecret'     => 'require',
+        'appsecret' => 'require',
         'token'     => 'require',
         '__token__' => 'token',
     ];
 
     protected $message = [
         'wxname.require'    => '名称不得为空',
+        'wxname.token'    => '名称不得为空',
         'appid.require'  => 'Appid不能为空',
         'appsecret.require'  => 'appsecret不得为空',
-        'token.require'  => 'token不能为空'
+        'token.require'  => 'token不能为空',
     ];
 
 }
